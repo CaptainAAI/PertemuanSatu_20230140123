1885115b2eef43781595911e08e874cf4032fdec
CaptainAAI <anandaagungismail@gmail.com>
2024-02-26 11:40:05 +0700

    Add project files.

---
+++ PertemuanSatu/PertemuanSatu.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Luas Lingkaran\n";
+
+    //begin
+        // numeric nJari, nLuas
+        // display 'Masukkan jejari= '
+        // accept nJari
+        // compute nLuas = 3.14 * r * r 
+        // display 'Luasnya= '+ nLuas
+    //end
+
+    double nJari, nLuas;
+    cout << "Masukkan jejari = ";
+    system("Pause");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
